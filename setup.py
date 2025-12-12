353c8572851882af0e7d7b6fa47d914782ffc97b
slymit <slymit@gmail.com>
2025-12-12 21:25:40 +0300

    Fix minimum version of sqlalchemy in install_requires

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     maintainer_email='slymit@gmail.com',
     url='https://github.com/slymit/sa-filters',
     packages=find_packages(exclude=['test', 'test.*']),
-    install_requires=['sqlalchemy>=1.4.0', 'setuptools'],
+    install_requires=['sqlalchemy>=1.4.24', 'setuptools'],
     extras_require={
         'dev': [
             'pytest>=6.2.5',
